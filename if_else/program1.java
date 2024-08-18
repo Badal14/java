79f474c60b723dd908bc98a5a57c7b84492008e0
Badal Lad <badallad67@gmail.com>
2024-08-18 11:31:20 +0530

    java program

---
+++ if_else/program1.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[] args){
+	
+		int value = 5;
+		if(value % 2 == 1){
+			System.out.println("value is odd" );
+		}else{
+			System.out.println("value is even" );
+
+		}
+	}
+}
